8b3ce7359cdaa73d6101219337f1a6e598184913
Cory G Watson <gphat@onemogin.com>
2014-09-06 18:03:15 -0700

    Parse CPU line

--- src/github.com/gphat/guru/cpu/cpu.go
+++ src/github.com/gphat/guru/cpu/cpu.go
@@ -53,13 +53,14 @@ func GetMetrics() defs.Response {
 
       // We can't do an exact switch because we need to do some matching
       // so we'll use a boolean
+      // TODO: interrupts? (intr)
       switch {
         case sigil == "ctxt":
           info["what"] = "ctxt"
           info["unit"] = "Event"
         case cpuLine.MatchString(sigil):
           // This one needs to be first because later we'll check for cpu
-          info["device"] = sigil // Put the cpu as the device
+          ParseCPULine(timestamp, metrics, parts)
           continue
         case strings.HasPrefix(sigil, "cpu"):
           // This catches the total CPU line
@@ -90,3 +91,46 @@ func GetMetrics() defs.Response {
   }
 }
 
+func ParseCPULine(timestamp time.Time, metrics []defs.Metric, parts []string) {
+
+  for i := 1; i < 10; i++ {
+    info := make(map[string]string)
+    info["unit"] = "Jiff"
+    info["device"] = parts[0]
+
+    floatval, fconverr := strconv.ParseFloat(parts[1], 64)
+    if fconverr != nil {
+      log.Printf("Cannot parse stat value '%v' as float64, skipping\n", parts[1])
+      continue
+    }
+
+    switch i {
+      case 1:
+        info["what"] = "user"
+      case 2:
+        info["what"] = "nice"
+      case 3:
+        info["what"] = "system"
+      case 4:
+        info["what"] = "idle"
+      case 5:
+        info["what"] = "iowait"
+      case 6:
+        info["what"] = "irq"
+      case 7:
+        info["what"] = "softirq"
+      case 8:
+        info["what"] = "steal"
+      case 9:
+        info["what"] = "guest"
+      case 10:
+        info["what"] = "guest_nice"
+    }
+
+    metrics = append(metrics, defs.Metric{
+      Timestamp:  timestamp,
+      Info:       info,
+      Value:      floatval,
+    })
+  }
+}
